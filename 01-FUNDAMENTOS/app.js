855419f05431c12e2ea23583c7cea6a32c6f6b16
Jose Manuel <jmdcarq89@gmail.com>
2021-01-02 12:27:49 +0100

    variables-com

--- 01-FUNDAMENTOS/app.js
+++ 01-FUNDAMENTOS/app.js
@@ -1,2 +1,23 @@
 
-console.log('Hola Mundo');
+// console.log('Hola Mundo');
+
+// let a = 10;
+// let b = 10;
+// let c = 10;
+// let d = 10;
+// let x = a + b;
+
+// let a = 10, b = 20, c = 10, d = 10, x = a + b;
+
+let a = 10, 
+    b = 20, 
+    c = 10, 
+    d = 10, 
+    x = a + b;
+
+
+
+
+
+
+console.log(x);
