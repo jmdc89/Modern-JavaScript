89977d7c56139b06fdc032f0a345c9844939c3ec
Jose Manuel <jmdcarq89@gmail.com>
2021-01-13 10:17:18 +0100

    ifelse-upd

--- 01-FUNDAMENTOS/assets/js/if-else.js
+++ 01-FUNDAMENTOS/assets/js/if-else.js
@@ -1,40 +1,40 @@
 
-let a = 5;
+// let a = 5;
 
 
-if ( a >= 10 ) { // undefined, null, una asignación
-    console.log('A es mayor o igual a 10');
-} else {
-    console.log('A es menor a 10');
-}
+// if ( a >= 10 ) { // undefined, null, una asignación
+//     console.log('A es mayor o igual a 10');
+// } else {
+//     console.log('A es menor a 10');
+// }
 
 
-// console.log('Fin de programa');
+// // console.log('Fin de programa');
 
 const hoy = new Date(); // {}
-let dia = hoy.getDay(); // 0: Domingo, 1: lunes, 2: martes.....
+// let dia = hoy.getDay(); // 0: Domingo, 1: lunes, 2: martes.....
 
-console.log({ dia });
+// console.log({ dia });
 
-if ( dia === 0 ) {
-    console.log('Domingo');
-} else if( dia === 1 ) {
-    console.log('Lunes');
+// if ( dia === 0 ) {
+//     console.log('Domingo');
+// } else if( dia === 1 ) {
+//     console.log('Lunes');
     
-    // if ( dia === 1 ) {
-    //     console.log('Lunes');
-    // } else {
-    //     console.log('No es lunes ni domingo');
-    // }
-} else if ( dia === 2 ){
-    console.log('Martes');
-} else {
-    console.log('No es lunes, martes o domingo...');
-}
+//     // if ( dia === 1 ) {
+//     //     console.log('Lunes');
+//     // } else {
+//     //     console.log('No es lunes ni domingo');
+//     // }
+// } else if ( dia === 2 ){
+//     console.log('Martes');
+// } else {
+//     console.log('No es lunes, martes o domingo...');
+// }
 
 
-// Sin usar If Else, o Switch, únicamente objetos
-dia = 6; // 0:domingo, 1: lunes....
+// // Sin usar If Else, o Switch, únicamente objetos
+// dia = 6; // 0:domingo, 1: lunes....
 
 const diasLetras = {
     0:'domingo',
@@ -47,7 +47,7 @@ const diasLetras = {
 }
 
 const diasLetras2 = ['domingo','lunes','martes','miércoles','jueves','viernes','sábado',];
-
+let dia2 = hoy.getDay();
 
 // día de la semana
-console.log( diasLetras2[dia] );
+console.log( diasLetras2[dia2] );
