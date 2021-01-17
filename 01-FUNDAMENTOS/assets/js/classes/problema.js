d8f1f6664650edcb4962edcef91049112ced28e8
Jose Manuel <jmdcarq89@gmail.com>
2021-01-17 12:02:02 +0100

    old classes

--- 01-FUNDAMENTOS/assets/js/classes/problema.js
+++ 01-FUNDAMENTOS/assets/js/classes/problema.js
@@ -15,3 +15,23 @@ const pedro = {
         console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);    
     }
 }
+
+// fher.imprimir();
+// Ok esto se debe de crear con la palabra new
+function Persona(nombre, edad) {
+    console.log('Se ejecutó esta línea');
+
+    this.nombre = nombre;
+    this.edad   = edad;   
+
+    this.imprimir = function() {
+        console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);    
+    }
+
+}
+
+const maria   = new Persona('María', 18);
+const melissa = new Persona('Melissa', 35);
+// console.log( maria );
+maria.imprimir();
+melissa.imprimir();
