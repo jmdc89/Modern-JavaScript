bb1e92affb4a6830296d1ba89a76650e46bc9ed3
Jose Manuel <jmdcarq89@gmail.com>
2021-01-17 11:57:38 +0100

    objects

---
+++ 01-FUNDAMENTOS/assets/js/classes/problema.js
@@ -0,0 +1,17 @@
+
+
+const fher = {
+    nombre: 'Fernando',
+    edad: 30,
+    imprimir() {
+        console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);    
+    }
+}
+
+const pedro = {
+    nombre: 'Pedro',
+    edad: 15,
+    imprimir() {
+        console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);    
+    }
+}
