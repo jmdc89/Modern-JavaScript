cd45e6587b3d55d76a6073c6faa5dd50580763a1
Jose Manuel <jmdcarq89@gmail.com>
2021-01-19 17:45:07 +0100

    Update index.js

--- 04-todo/src/index.js
+++ 04-todo/src/index.js
@@ -1,10 +1,11 @@
 import './styles.css';
 
 import { Todo, TodoList } from './classes';
-import { createTodoHtml } from './js/componentes';
-// import { Todo } from './classes/todo.class.js';
-// import { TodoList } from './classes/todo-list.class';
+import { crearTodoHtml } from './js/componentes';
+
 
 export const todoList = new TodoList();
 
-todoList.todos.forEach( todo => createTodoHtml(todo));
+todoList.todos.forEach( crearTodoHtml );
+
+console.log( 'todos', todoList.todos );
