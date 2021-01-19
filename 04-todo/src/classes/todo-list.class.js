7a08c3ddc02a4c131c4b4b54f3dd564981356614
Jose Manuel <jmdcarq89@gmail.com>
2021-01-19 11:00:57 +0100

    TodoList - class + index

---
+++ 04-todo/src/classes/todo-list.class.js
@@ -0,0 +1,24 @@
+
+export class TodoList {
+
+    constructor() {
+
+        this.todos = [];
+    }
+
+    newTodo( todo ) {
+        this.todos.push( todo );
+    }
+
+    deleteTodo( id ) {
+
+    }
+
+    toggleTodo( id ) {
+
+    }
+
+    deleteCompleted() {
+
+    }
+}
