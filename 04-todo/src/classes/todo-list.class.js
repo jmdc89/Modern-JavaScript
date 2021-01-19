8f07ea2a6c7a7904bb6e17354685931ba98e0c0d
Jose Manuel <jmdcarq89@gmail.com>
2021-01-19 17:44:23 +0100

    Update todo-list-class

--- 04-todo/src/classes/todo-list.class.js
+++ 04-todo/src/classes/todo-list.class.js
@@ -1,54 +1,54 @@
-import {Todo} from './todo.class';
+import { Todo } from './todo.class';
 
 export class TodoList {
 
     constructor() {
 
         // this.todos = [];
-        this.chargeLocalStorage();
+        this.cargarLocalStorage();
+
     }
 
-    newTodo( todo ) {
+    nuevoTodo( todo ) {
         this.todos.push( todo );
-        this.saveLocalStorage();
+        this.guardarLocalStorage();
     }
 
-    deleteTodo( id ) {
+    eliminarTodo( id ) {
 
         this.todos = this.todos.filter( todo => todo.id != id )
-        this.saveLocalStorage();
-
+        this.guardarLocalStorage();
     }
 
-    toggleTodo( id ) {
+    marcarCompletado( id ) { 
 
         for( const todo of this.todos ) {
 
-            if ( todo.id == id ) {
+            if( todo.id == id ) {
 
                 todo.completado = !todo.completado;
-                this.saveLocalStorage();
+                this.guardarLocalStorage();
                 break;
-
             }
+
         }
 
 
     }
 
-    deleteCompleted() {
-
+    eliminarCompletados() {
+        
         this.todos = this.todos.filter( todo => !todo.completado )
-
+        this.guardarLocalStorage();
     }
 
-    saveLocalStorage(){
+    guardarLocalStorage(){
 
         localStorage.setItem('todo', JSON.stringify( this.todos ) );
         
     }
 
-    chargeLocalStorage(){
+    cargarLocalStorage(){
 
         this.todos = ( localStorage.getItem('todo') )
                         ? JSON.parse( localStorage.getItem('todo') )
@@ -56,4 +56,5 @@ export class TodoList {
         
         this.todos = this.todos.map( Todo.fromJson );
     }
-};
+
+}
